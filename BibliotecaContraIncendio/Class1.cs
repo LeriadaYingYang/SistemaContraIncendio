c2514b1d75bdb56b046a6142204319cf227c6cbc
Quiroz Cabanillas Franco Yaren <N00500542>
2025-12-01 01:12:51 -0500

    Agregando los limites que se estan poniendo para nuestro texto y limite de temperatura

--- BibliotecaContraIncendio/Class1.cs
+++ BibliotecaContraIncendio/Class1.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BibliotecaContraIncendio
+namespace BibliotecaContraIncendios
 {
-    public class Class1
+    public class OperacionesAlarma
     {
+        public static int[] historialtemperatura = new int[100];
+        public static string[] historialFechas = new string[100];
+        public static int cantidad = 0;
+
+        public static int limiteAlerta = 47;
+        public static int limitePeligro = 80;
     }
 }
